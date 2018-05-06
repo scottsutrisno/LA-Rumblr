f791839565306cc11243895a1e4f1f8e4811eb4d
Scott Sutrisno <scottsutrisno@gmail.com>
2018-05-06 17:42:23 -0400

    Add test user and ability to edit profile photo link

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 User.create(username: "mokaymokay", password: "muggles", email: "kay@example.com")
-Profile.create(first_name: "Kay", last_name: "Mok", birthday: 1991-12-10, about_me: "hi", avatar: "", gender: "Female", user_id: 1)
+Profile.create(first_name: "Kay", last_name: "Mok", birthday: DateTime.new(1991,12,10), about_me: "hi", avatar: "", user_id: 1)
